48976b9fe0a4bf9967de168caeab4b28e46d85aa
peter3 <peter3@68f86b67-9755-4d40-9bad-d95cc7b15546>
2006-12-04 19:13:41 +0000

    Tag for latest version

---
+++ translator/tags/release_1.9.0.41/src/PolyglotPlugin/PolyGlotParserImpl.pas
@@ -0,0 +1,265 @@
+unit PolyGlotParserImpl;
+{
+  Copyright © 2004 by Chris Thornton, Thornsoft Development, Inc.; all rights reserved
+
+  Purpose:
+   This parser allows native import/export for Polyglot files.  Polyglot is
+   a Delphi "ini translator" by Balmsoft - www.balmsoft.com.
+   The primary job of this parser is to strip/add single quotes.
+   ex:
+     Polyglot:   64638_arm_R_ARM_ERROR_ACCOUNT_EXPIRED='Key Expired'
+     IniTrans:   64638_arm_R_ARM_ERROR_ACCOUNT_EXPIRED=Key Expired
+     Polyglot:   ShowQuickBar1.Hint='Open New Preview/Edit Window'#13#10'(Multiple instances allowed)'#13#10'While there is already one of these docked into ClipMate Explorer, '#13#10'you can open as many as you''d like.'
+     IniTrans:   ShowQuickBar1.Hint=Open New Preview/Edit Window#13#10(Multiple instances allowed)#13#10While there is already one of these docked into ClipMate Explorer, #13#10you can open as many as youd like.
+
+  Developer(s):
+   Chris Thornton - chris at thornsoft dot com
+
+  Based On:  Oleg Parser
+
+  Status:
+   The contents of this file are subject to the Mozilla Public License Version
+   1.1 (the "License"); you may not use this file except in compliance with the
+   License. You may obtain a copy of the License at http://www.mozilla.org/MPL/MPL-1.1.html
+
+   Software distributed under the License is distributed on an "AS IS" basis,
+   WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+   the specific language governing rights and limitations under the License.
+}
+
+interface
+uses
+  Classes, Types, TntClasses, TransIntf;
+
+type
+  TPolyGlotParser = class(TInterfacedObject, IUnknown, IFileParser)
+  private
+    FOldAppHandle: Cardinal;
+    FTransFile, FOrigFile: string;
+    FSection: WideString;
+    procedure LoadSettings;
+    procedure SaveSettings;
+    procedure BuildPreview(Items: ITranslationItems; Strings: TTntStrings);
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    function Capabilities: Integer; safecall;
+    function Configure(Capability: Integer): HRESULT; safecall;
+    function DisplayName(Capability: Integer): WideString; safecall;
+    function ExportItems(const Items: ITranslationItems;
+      const Orphans: ITranslationItems): HRESULT; safecall;
+    function ImportItems(const Items: ITranslationItems;
+      const Orphans: ITranslationItems): HRESULT; safecall;
+    procedure Init(const ApplicationServices: IApplicationServices); safecall;
+  end;
+
+implementation
+uses
+  SysUtils, Forms, IniFiles, PreviewExportFrm, DualImportFrm;
+
+const
+  cPolyGlotFilter = 'PolyGlot Translator  Files (*.lng)|*.lng|All files (*.*)|*.*';
+  cPolyGlotImportTitle = 'Import from PolyGlot Translator file';
+  cPolyGlotExportTitle = 'Export to PolyGlot Translator file';
+  //cSectionName = 'PolyGlot Translator';
+
+{ TPolyGlotParser }
+
+procedure TPolyGlotParser.BuildPreview(Items: ITranslationItems; Strings: TTntStrings);
+var i: integer;
+begin
+  FSection := '';
+  for i := 0 to Items.Count - 1 do
+  begin
+      //Next line is Useful for debugging
+      //Strings.Add(Format('Section:[%s], Name[%s], Original[%s], Trans[%s]',[Items[i].Section, Items[i].Name, Items[i].Original, Items[i].Translation]));
+    if (FSection <> Items[i].Section) then // output [Section] on line by itself, on section break.
+    begin
+      if (FSection <> '') then
+        Strings.Add(''); // Throw in a blank line between sections.
+      Strings.Add(Format('[%s]', [Items[i].Section]));
+      FSection := Items[i].Section;
+    end;
+    if Items[i].Translation <> '' then
+      Strings.Add(Items[i].Name + '=' + chr(39) + StringReplace(Items[i].Translation, '#13#10', chr(39) + '#13#10' + chr(39), [rfReplaceAll]) + chr(39));
+  end;
+end;
+
+function TPolyGlotParser.Capabilities: Integer;
+begin
+  Result := CAP_IMPORT or CAP_EXPORT;
+end;
+
+function TPolyGlotParser.Configure(Capability: Integer): HRESULT;
+begin
+  Result := E_NOTIMPL;
+end;
+
+constructor TPolyGlotParser.Create;
+begin
+  inherited;
+  FOldAppHandle := Application.Handle;
+end;
+
+destructor TPolyGlotParser.Destroy;
+begin
+  if FOldAppHandle <> 0 then
+    Application.Handle := FOldAppHandle;
+  inherited;
+end;
+
+function TPolyGlotParser.DisplayName(Capability: Integer): WideString;
+begin
+  case Capability of
+    CAP_IMPORT:
+      Result := cPolyGlotImportTitle;
+    CAP_EXPORT:
+      Result := cPolyGlotExportTitle;
+  else
+    Result := '';
+  end;
+end;
+
+function TPolyGlotParser.ExportItems(const Items,
+  Orphans: ITranslationItems): HRESULT;
+var
+  S: TTntStringlist;
+  FOldSort: TTranslateSortType;
+begin
+  Result := S_FALSE;
+  FOldSort := Items.Sort;
+  FSection := ''; // init
+  try
+    LoadSettings;
+    Items.Sort := stIndex;
+    S := TTntStringlist.Create;
+    try
+      BuildPreview(Items, S);
+      if TfrmExport.Execute(FTransFile, cPolyGlotExportTitle, cPolyGlotFilter, '.', 'lng', S) then
+      begin
+        S.AnsiStrings.SaveToFile(FTransFile);
+        Result := S_OK;
+        SaveSettings;
+      end;
+    finally
+      S.Free;
+      Items.Sort := FOldSort;
+    end;
+  except
+    Application.HandleException(self);
+  end;
+end;
+
+function TPolyGlotParser.ImportItems(const Items, Orphans: ITranslationItems): HRESULT;
+var
+  S: TTntStringlist;
+  i, j, k: integer;
+  TI: ITranslationItem;
+begin
+  Result := S_FALSE;
+  try
+    Items.Clear;
+    Orphans.Clear;
+    TI := nil;
+    LoadSettings;
+    if TfrmDualImport.Execute(FOrigFile, FTransFile, cPolyGlotImportTitle, cPolyGlotFilter, '.', 'lng', True) then
+    begin
+      Items.Sort := stNone;
+      S := TTntStringlist.Create;
+      try
+        S.LoadFromFile(FOrigFile);
+        for i := 0 to S.Count - 1 do
+        begin
+          if Pos('[', S[i]) = 1 then
+            FSection := Copy(S[i], 2, Pos(']', S[i]) - 2) //New Section
+          else
+          begin
+            j := Pos('=', S[i]);
+            if j > 0 then
+            begin
+              TI := Items.Add;
+              TI.Section := FSection;
+
+              TI.Name := Copy(S[i], 1, j - 1);
+              TI.Original := StringReplace(Copy(S[i], j + 1, MaxInt), #39, '', [rfReplaceAll]);
+              TI.Translation := '';
+            end;
+          end;
+        end;
+        {Now load the translation. Need to sync with the original}
+        if FileExists(FTransFile) then {For new projects, there won't be one.}
+        begin
+          S.LoadFromFile(FTransFile);
+          for i := 0 to S.Count - 1 do
+          begin
+            if Pos('[', S[i]) = 1 then
+              FSection := Copy(S[i], 2, Pos(']', S[i]) - 2) //New Section
+            else
+            begin
+              j := Pos('=', S[i]);
+              if j > 0 then
+              begin
+                k := Items.IndexOf(FSection, Copy(S[i], 1, j - 1)); {Find corresponding entry in Original file}
+                if (k > -1) then
+                  TI := Items.Items[k]
+                else
+                begin {Not found - somehow....}
+                  TI := Items.Add;
+                  TI.Section := FSection;
+                  TI.Name := Copy(S[i], 1, j - 1);
+                  TI.Original := '';
+                end;
+                TI.Translation := StringReplace(Copy(S[i], j + 1, MaxInt), #39, '', [rfReplaceAll]);
+              end;
+            end;
+          end;
+        end;
+      finally
+        Items.Sort := stIndex;
+        S.Free;
+      end;
+      SaveSettings;
+      Result := S_OK;
+    end;
+  except
+    Application.HandleException(self);
+  end;
+end;
+
+procedure TPolyGlotParser.Init(const ApplicationServices: IApplicationServices);
+begin
+  Application.Handle := ApplicationServices.AppHandle;
+end;
+
+procedure TPolyGlotParser.LoadSettings;
+begin
+  try
+    with TIniFile.Create(ChangeFileExt(GetModuleName(hInstance), '.ini')) do
+    try
+      FOrigFile := ReadString('Settings', 'OrigFile', FOrigFile);
+      FTransFile := ReadString('Settings', 'TransFile', FTransFile);
+    finally
+      Free;
+    end;
+  except
+    Application.HandleException(self);
+  end;
+end;
+
+procedure TPolyGlotParser.SaveSettings;
+begin
+  try
+    with TIniFile.Create(ChangeFileExt(GetModuleName(hInstance), '.ini')) do
+    try
+      WriteString('Settings', 'OrigFile', FOrigFile);
+      WriteString('Settings', 'TransFile', FTransFile);
+    finally
+      Free;
+    end;
+  except
+    Application.HandleException(self);
+  end;
+end;
+
+end.
---
+++ translator/tags/release_1.9.0.41/src/SciTEPlugin/SciTEParserImpl.pas
@@ -0,0 +1,305 @@
+{@abstract(Parser for SciTE language files) }
+{
+  Copyright © 2003-2006 by Peter Thornqvist; all rights reserved
+
+  Developer(s):
+    p3 - peter3 att users dott sourceforge dott net
+
+  Status:
+   The contents of this file are subject to the Mozilla Public License Version
+   1.1 (the "License"); you may not use this file except in compliance with the
+   License. You may obtain a copy of the License at http://www.mozilla.org/MPL/MPL-1.1.html
+
+   Software distributed under the License is distributed on an "AS IS" basis,
+   WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+   the specific language governing rights and limitations under the License.
+}
+// $Id$
+unit SciTEParserImpl;
+
+interface
+uses
+  SysUtils, Classes, Types, TntClasses, TransIntf;
+{
+RESTRICTIONS:
+  * strips all comments
+  * does not support embedded quotes
+  * does not do any character escaping
+}
+
+
+type
+  TSciTEParser = class(TInterfacedObject, IUnknown, IFileParser)
+  private
+    FOldHandle: LongWord;
+    FTransFile: string;
+    FExportRect: TRect;
+    procedure BuildPreview(const Items: ITranslationItems; Strings: TTntStrings);
+    function DoSciTEImport(const Items, Orphans: ITranslationItems; const TransFile: string): boolean;
+    procedure LoadSettings;
+    procedure SaveSettings;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    function Configure(Capability: Integer): HRESULT; safecall;
+    function DisplayName(Capability: Integer): WideString; safecall;
+    function ExportItems(const Items, Orphans: ITranslationItems): HRESULT; safecall;
+    function ImportItems(const Items, Orphans: ITranslationItems): HRESULT; safecall;
+    procedure Init(const ApplicationServices: IApplicationServices); safecall;
+    function Capabilities: Integer; safecall;
+  end;
+
+implementation
+uses
+  Windows, Forms, IniFiles, PreviewExportFrm, SingleImportFrm;
+
+const
+  cSciTEFilter = 'SciTE files (locale.properties)|*.properties|All files (*.*)|*.*';
+  cSciTEExportTitle = 'Export to SciTE language file';
+  cSciTEImportTitle = 'Import from SciTE language file';
+  cSectionName = 'SciTE';
+  SImportError = 'There was an error importing, please check the files and try again';
+  SError = 'SciTE Parser Error';
+
+function YesNo(const Text, Caption: string): boolean;
+begin
+  Result := Application.MessageBox(PChar(Text), PChar(Caption), MB_YESNO or MB_ICONQUESTION) = IDYES;
+end;
+
+procedure ShowError(const Text: string);
+begin
+  Application.MessageBox(PChar('There was an error:'#13#10 + Text), PChar('SciTE Error'), MB_OK or MB_ICONERROR);
+end;
+
+{ TSciTEParser }
+
+function TSciTEParser.Capabilities: Integer;
+begin
+  Result := CAP_IMPORT or CAP_EXPORT;
+end;
+
+function TSciTEParser.Configure(Capability: Integer): HRESULT;
+begin
+  Result := S_OK;
+end;
+
+constructor TSciTEParser.Create;
+begin
+  inherited Create;
+  FOldHandle := Application.Handle;
+end;
+
+procedure TSciTEParser.BuildPreview(const Items: ITranslationItems;
+  Strings: TTntStrings);
+var
+  i: integer;
+  FOldSort: TTranslateSortType;
+begin
+  try
+    FOldSort := Items.Sort;
+    try
+      Items.Sort := stIndex;
+      for i := 0 to Items.Count - 1 do
+      begin
+        if Items[i].OrigComments <> '' then
+          Strings.Add(Items[i].OrigComments);
+        Strings.Add(Format('%s=%s', [Items[i].Original, Items[i].Translation]));
+      end;
+    finally
+      Items.Sort := FOldSort;
+    end;
+  except
+    Application.HandleException(Self);
+  end;
+end;
+
+destructor TSciTEParser.Destroy;
+begin
+  Application.Handle := FOldHandle;
+  inherited;
+end;
+
+function TSciTEParser.DisplayName(Capability: Integer): WideString;
+begin
+  case Capability of
+    CAP_IMPORT: Result := cSciTEImportTitle;
+    CAP_EXPORT: Result := cSciTEExportTitle;
+//    CAP_CONFIGURE : Result := 'Configure';
+  else
+    Result := '';
+  end;
+end;
+
+function TSciTEParser.DoSciTEImport(const Items, Orphans: ITranslationItems;
+  const TransFile: string): boolean;
+var
+  S: TTntStringList;
+  i: integer;
+  FOldSort: TTranslateSortType;
+  FLastItem: ITranslationItem;
+  procedure ParseRow(const S: WideString);
+  begin
+    if (Pos('#', S) = 1) then
+    begin
+      if FLastItem = nil then
+      begin
+        FLastItem := Items.Add;
+        FLastItem.Index := Items.Count - 1;
+      end;
+      if FLastItem.OrigComments <> '' then
+        FLastItem.OrigComments := FLastItem.OrigComments + #13#10;
+      FLastItem.OrigComments := FLastItem.OrigComments + S;
+    end
+    else if (S = '') then
+    begin
+      if FLastItem = nil then
+      begin
+        FLastItem := Items.Add;
+        FLastItem.Index := Items.Count - 1;
+      end;
+      FLastItem.OrigComments := FLastItem.OrigComments + #13#10;
+    end
+    else
+    begin
+      if FLastItem = nil then
+      begin
+        FLastItem := Items.Add;
+        FLastItem.Index := Items.Count - 1;
+      end;
+      FLastItem.Section := 'SciTE';
+      FLastItem.Original := Copy(S, 1, Pos('=', S) - 1);
+      FLastItem.Name := FLastItem.Original;
+      FLastItem.Translation := Copy(S, Pos('=', S) + 1, MaxInt);
+      FLastItem.Translated := FLastItem.Translation <> '';
+      FLastItem := nil;
+    end;
+  end;
+begin
+  Result := false;
+  try
+    Items.Clear;
+    Orphans.Clear;
+    FOldSort := Items.Sort;
+    Items.Sort := stNone;
+    FLastItem := nil;
+    S := TTntStringlist.Create;
+    try
+      S.LoadFromFile(TransFile);
+      for i := 0 to S.Count - 1 do
+        ParseRow(S[i]);
+      // remove any empty lines at the end
+      i := Items.Count - 1;
+      while (i >= 0) and (Items[i].Original = '') do
+      begin
+        Items.Delete(i);
+        Dec(i);
+      end;
+    finally
+      S.Free;
+    end;
+    Items.Sort := FOldSort;
+    Result := true;
+  except
+    Application.HandleException(self);
+  end;
+end;
+
+function TSciTEParser.ExportItems(const Items, Orphans: ITranslationItems): HRESULT;
+var S: TTntStringlist;
+begin
+  try
+    Result := S_FALSE;
+    LoadSettings;
+    S := TTntStringlist.Create;
+    try
+      BuildPreview(Items, S);
+      if TfrmExport.Execute(FTransFile, cSciTEExportTitle, cSciTEFilter, '.', 'properties', S) then
+      begin
+        S.AnsiStrings.SaveToFile(FTransFile);
+        SaveSettings;
+        Result := S_OK;
+      end;
+    finally
+      S.Free
+    end;
+  except
+    Application.HandleException(Self);
+  end;
+end;
+
+function TSciTEParser.ImportItems(const Items, Orphans: ITranslationItems): HRESULT;
+begin
+  try
+    Result := S_FALSE;
+    LoadSettings;
+    if TfrmSingleImport.Execute(FTransFile, cSciTEImportTitle, cSciTEFilter, '.', 'properties') then
+    begin
+      if DoSciTEImport(Items, Orphans, FTransFile) then
+      begin
+        SaveSettings;
+        Result := S_OK;
+      end
+      else
+        Application.MessageBox(PChar(SImportError), PChar(SError), MB_OK or MB_ICONERROR);
+    end;
+  except
+    Application.HandleException(Self);
+  end;
+end;
+
+procedure TSciTEParser.Init(const ApplicationServices: IApplicationServices);
+begin
+  Application.Handle := ApplicationServices.AppHandle;
+end;
+
+procedure TSciTEParser.LoadSettings;
+var M: TMemoryStream;
+begin
+  try
+    with TIniFile.Create(ChangeFileExt(GetModuleName(hInstance), '.ini')) do
+    try
+      FTransFile := ReadString('Settings', 'TransFile', FTransFile);
+      M := TMemoryStream.Create;
+      try
+        if ReadBinaryStream('Forms', 'Export', M) = SizeOf(TRect) then
+        begin
+          M.Seek(0, soFromBeginning);
+          Move(M.Memory^, Pointer(@FExportRect)^, sizeof(TRect));
+        end;
+      finally
+        M.Free;
+      end;
+    finally
+      Free;
+    end;
+  except
+    Application.HandleException(Self);
+  end;
+end;
+
+procedure TSciTEParser.SaveSettings;
+var M: TMemoryStream;
+begin
+  try
+    with TIniFile.Create(ChangeFileExt(GetModuleName(hInstance), '.ini')) do
+    try
+      WriteString('Settings', 'TransFile', FTransFile);
+      M := TMemoryStream.Create;
+      try
+        M.Write(FExportRect, sizeof(TRect));
+        M.Seek(0, soFromBeginning);
+        WriteBinaryStream('Forms', 'Export', M);
+      finally
+        M.Free;
+      end;
+    finally
+      Free;
+    end;
+  except
+    Application.HandleException(Self);
+  end;
+end;
+
+
+end.
+

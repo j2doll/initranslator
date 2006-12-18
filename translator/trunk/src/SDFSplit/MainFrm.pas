f37931d0a624401f18a807c7c1a277795cb562b4
peter3 <peter3@68f86b67-9755-4d40-9bad-d95cc7b15546>
2006-12-18 18:30:59 +0000

    - SDFSplit: New utility for splitting and extracting languages from OpenOffice multi-language sdf files

---
+++ translator/trunk/src/SDFSplit/MainFrm.pas
@@ -0,0 +1,319 @@
+unit MainFrm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TfrmMain = class(TForm)
+    Label1: TLabel;
+    edSDFFile: TEdit;
+    btnSDFFile: TButton;
+    Label2: TLabel;
+    edSaveFolder: TEdit;
+    btnSaveFolder: TButton;
+    btnOK: TButton;
+    btnCancel: TButton;
+    odSDFFile: TOpenDialog;
+    chkExtractLanguage: TCheckBox;
+    cbLanguages: TComboBox;
+    chkSortItems: TCheckBox;
+    procedure btnOKClick(Sender: TObject);
+    procedure btnCancelClick(Sender: TObject);
+    procedure btnSDFFileClick(Sender: TObject);
+    procedure btnSaveFolderClick(Sender: TObject);
+    procedure chkExtractLanguageClick(Sender: TObject);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+    procedure LoadSettings;
+    procedure SaveSettings;
+    procedure SplitFile(const Filename, OutputFolder: string; SortItems: boolean);
+    procedure ExtractFile(const Filename, OutputFolder, Language: string; SortItems: boolean);
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+uses
+  FileCtrl, IniFiles;
+
+{$R *.dfm}
+
+procedure StrTokenize(const S: string; Delimiter: Char; List: TStrings; MinLength: integer = 1);
+var i, j: integer;
+  tmp: string;
+begin
+  j := 1;
+  for i := 1 to Length(S) do
+  begin
+    if (S[i] = Delimiter) then
+    begin
+      tmp := Copy(S, j, i - j);
+      if Length(tmp) >= MinLength then
+        List.Add(tmp);
+      j := i + 1;
+    end;
+  end;
+  if j <= Length(S) then
+  begin
+    tmp := Copy(S, j, MaxInt);
+    if Length(tmp) >= MinLength then
+      List.Add(tmp);
+  end;
+end;
+
+procedure TfrmMain.btnOKClick(Sender: TObject);
+begin
+  if not FileExists(edSDFFile.Text) then
+    ShowMessage('File does not exist')
+  else if not DirectoryExists(edSaveFolder.Text) then
+    ShowMessage('Output folder does not exist')
+  else if chkExtractLanguage.Checked then
+  begin
+    if cbLanguages.Text = '' then
+    begin
+      ShowMessage('No language specified');
+      Exit;
+    end;
+    ExtractFile(edSDFFile.Text, edSaveFolder.Text, cbLanguages.Text, chkSortItems.Checked);
+    ShowMessage('Done');
+  end
+  else
+  begin
+    SplitFile(edSDFFile.Text, edSaveFolder.Text, chkSortItems.Checked);
+    ShowMessage('Done');
+  end;
+end;
+
+function GetLanguage(S: string): string;
+var T: TStringlist;
+begin
+  T := TStringlist.Create;
+  try
+    StrTokenize(S, #9, T, 0);
+    if T.Count > 9 then
+      Result := T[9]
+    else
+      Result := '';
+  finally
+    T.Free;
+  end;
+end;
+
+procedure TfrmMain.SplitFile(const Filename, OutputFolder: string; SortItems: boolean);
+var
+  i, aStart, aIndex, aCount: integer;
+  S: TStringlist;
+  aFileNameTemplate, aBaseFolder, aCurrentFilename: string;
+  FFiles: array of TextFile;
+  FLanguages: TStringlist;
+
+  function GetLanguages(S: TStrings; i: integer): integer;
+  var
+    j: integer;
+    lang: string;
+  begin
+    for j := i to S.Count - 1 do
+    begin
+      lang := GetLanguage(S[j]);
+      if FLanguages.IndexOf(lang) > -1 then
+      begin
+        Result := FLanguages.Count;
+        Exit;
+      end
+      else
+        FLanguages.Add(lang);
+    end;
+    Result := 0;
+  end;
+begin
+  aBaseFolder := IncludeTrailingPathDelimiter(OutputFolder);
+  aFileNameTemplate := ExtractFileName(Filename);
+  S := TStringlist.Create;
+  FLanguages := TStringlist.Create;
+  try
+    FLanguages.Sorted := true;
+    S.LoadFromFile(Filename);
+    aStart := 0;
+  // skip empty and comment lines
+    while aStart < S.Count do
+      if (S[aStart] = '') or (S[aStart][1] = '#') then
+        Inc(aStart)
+      else
+        Break;
+    aCount := GetLanguages(S, aStart);
+    if aCount > 0 then
+    begin
+      SetLength(FFiles, aCount);
+      for i := 0 to aCount - 1 do
+      begin
+        // create a folder for each file, named like the language
+        ForceDirectories(aBaseFolder + FLanguages[i]);
+        aCurrentFilename := IncludeTrailingPathDelimiter(aBaseFolder + FLanguages[i]) + aFileNameTemplate;
+        // assign the filename to the file
+        // this assures that the index of a file in FFiles directly syncs up with the items in FLanguages,
+        // i.e they have the same index - THIS IS VERY IMPORTANT!
+        AssignFile(FFiles[i], aCurrentFilename);
+        Rewrite(FFiles[i]);
+      end;
+      // walk the source file, extract lines and put into correct file based on the language
+      for i := aStart to S.Count - 1 do
+      begin
+        aIndex := FLanguages.IndexOf(GetLanguage(S[i]));
+        if aIndex >= 0 then
+          Writeln(FFiles[aIndex], S[i]);
+      end;
+
+      if SortItems then
+      begin
+        // save the file names for later
+        FLanguages.Clear;
+        for i := 0 to aCount - 1 do
+          FLanguages.Add(TTextRec(FFiles[i]).Name);
+      end;
+      // close the files
+      for i := 0 to aCount - 1 do
+      begin
+        Flush(FFiles[i]);
+        CloseFile(FFiles[i]);
+      end;
+      SetLength(FFiles, 0);
+      if SortItems then
+      // sort the files
+        for i := 0 to aCount - 1 do
+        begin
+          S.LoadFromFile(FLanguages[i]);
+          S.Sort;
+          S.SaveToFile(FLanguages[i]);
+        end;
+    end;
+  finally
+    S.Free;
+    FLanguages.Free;
+  end;
+end;
+
+procedure TfrmMain.ExtractFile(const Filename, OutputFolder, Language: string; SortItems: boolean);
+var
+  i, aStart: integer;
+  S: TStringlist;
+  aFileNameTemplate, aBaseFolder, aCurrentFilename: string;
+  FFile: TextFile;
+begin
+  aBaseFolder := IncludeTrailingPathDelimiter(OutputFolder);
+  aFileNameTemplate := ExtractFileName(Filename);
+  S := TStringlist.Create;
+  try
+    S.LoadFromFile(Filename);
+    aStart := 0;
+  // skip empty and comment lines
+    while aStart < S.Count do
+      if (S[aStart] = '') or (S[aStart][1] = '#') then
+        Inc(aStart)
+      else
+        Break;
+    ForceDirectories(aBaseFolder + Language);
+    aCurrentFilename := IncludeTrailingPathDelimiter(aBaseFolder + Language) + aFileNameTemplate;
+    AssignFile(FFile, aCurrentFilename);
+    Rewrite(FFile);
+    try
+      for i := aStart to S.Count - 1 do
+        if AnsiSameText(Language, GetLanguage(S[i])) then
+          Writeln(FFile, S[i]);
+    finally
+      CloseFile(FFile);
+    end;
+    if SortItems then
+    begin
+      S.LoadFromFile(aCurrentFilename);
+      S.Sort;
+      S.SaveToFile(aCurrentFilename);
+    end;
+  finally
+    S.Free;
+  end;
+end;
+
+procedure TfrmMain.btnCancelClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TfrmMain.btnSDFFileClick(Sender: TObject);
+begin
+  odSDFFile.FileName := edSDFFile.Text;
+  if odSDFFile.Execute then
+  begin
+    edSDFFile.Text := odSDFFile.FileName;
+    if not DirectoryExists(edSaveFolder.Text) then
+      edSaveFolder.Text := ExtractFilePath(odSDFFile.FileName);
+  end;
+end;
+
+procedure TfrmMain.btnSaveFolderClick(Sender: TObject);
+var S: string;
+begin
+  S := edSaveFolder.Text;
+  if S = '' then
+    S := ExtractFilePath(edSDFFile.Text);
+  if SelectDirectory('Select output folder', '', S) then
+    edSDFFile.Text := S;
+end;
+
+procedure TfrmMain.chkExtractLanguageClick(Sender: TObject);
+begin
+  if chkExtractLanguage.Checked then
+    btnOK.Caption := 'Extract'
+  else
+    btnOK.Caption := 'Split';
+  cbLanguages.Enabled := chkExtractLanguage.Checked;
+end;
+
+procedure TfrmMain.LoadSettings;
+begin
+  with TIniFile.Create(ChangeFileExt(Application.Exename, '.ini')) do
+  try
+    edSDFFile.Text := ReadString('Settings', 'Filename','');
+    edSaveFolder.Text := ReadString('Settings', 'SaveFolder','');
+    chkExtractLanguage.Checked := ReadBool('Settings', 'ExtractLanguage',false);
+    chkSortItems.Checked := ReadBool('Settings', 'SortItems', true);
+    cbLanguages.Text := ReadString('Settings', 'Language','af');
+  finally
+    Free;
+  end;
+end;
+
+procedure TfrmMain.SaveSettings;
+begin
+  with TIniFile.Create(ChangeFileExt(Application.Exename, '.ini')) do
+  try
+    WriteString('Settings', 'Filename',edSDFFile.Text);
+    WriteString('Settings', 'SaveFolder',edSaveFolder.Text);
+    WriteBool('Settings', 'ExtractLanguage',chkExtractLanguage.Checked);
+    WriteBool('Settings', 'SortItems', chkSortItems.Checked);
+    WriteString('Settings', 'Language',cbLanguages.Text);
+  finally
+    Free;
+  end;
+end;
+
+procedure TfrmMain.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+begin
+  SaveSettings;
+end;
+
+procedure TfrmMain.FormShow(Sender: TObject);
+begin
+  LoadSettings;
+  OnShow := nil;
+end;
+
+end.
+
